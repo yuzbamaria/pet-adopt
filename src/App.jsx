a76e341e3ea3c62485b3d5ecb0fa4ec2a6b0abcb
Saif Uddin <admin@saifuddin.uk>
2024-02-08 12:37:54 +0000

    added header, wrapper, footer components and home page to start testing for login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import React from "react";
 import './App.css'
+import Home from "./pages/Home"
+import Header from "./components/Header"
+import Wrapper from "./components/Wrapper"
+import Footer from "./components/Footer"
 
 function App() {
-  return (<></>)
+  return (<div>
+    <Header />
+    <Wrapper>
+      <Home />
+    </Wrapper>
+    <Footer />
+  </div>)
 }
 
 export default App
---
+++ src/components/Wrapper/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Wrapper(props) {
+    return(<main>
+        {props.children}
+    </main>)
+}
+
+export default Wrapper
