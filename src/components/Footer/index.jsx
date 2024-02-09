934596b4f79d30e421b52a47c5ea72bc6edf464d
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-02-09 14:29:28 +0200

    updated footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-function Footer(props) {
-    return(<footer>
-        Footer
-    </footer>)
+// function Footer(props) {
+//     return(<footer>
+//         Footer
+//     </footer>)
+// }
+
+// export default Footer
+
+
+const currentYear = new Date().getFullYear();
+
+function Footer() {
+  return (
+    
+    <footer className="footer text-center d-flex justify-content-center align-items-center pb-2">
+      <p><span id="current-year">©</span>
+      {currentYear} Maria Yuzba & Saif Uddin. All rights reserved.
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
