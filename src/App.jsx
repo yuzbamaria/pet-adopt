04c3004893a5b2948effa0a0d62fcad844911207
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-02-11 02:39:26 +0200

    added logic to handle search and rendering a list of video titles from  Youtube API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css'
-import Header from "./components/Header"
-import Wrapper from "./components/Wrapper"
-import Footer from "./components/Footer"
+import './App.css';
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (<Router>
     <Header />
     <Wrapper />
-
+    
     <Footer />
   </Router>)
 }
 
-export default App
+export default App;
---
+++ src/components/YoutubeAPIResult/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+function YoutubeAPI() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [videos, setVideos] = useState([]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchVideos = async () => {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCfM1E9frJRQ29BTnI2c5HjMLvbHPbT66Y&q=${searchTerm}&part=snippet&type=video`
+      );
+      const data = await response.json();
+      setVideos(data.items);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Enter keywords"
+        />
+        <button onClick={searchVideos}>Search</button>
+      </div>
+      <ul>
+        {videos.map((video) => (
+          <li key={video.id.videoId}>
+            <a
+              href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {video.snippet.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default YoutubeAPI;
