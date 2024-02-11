41b8f9da4bfa083878163fc1c5dbeb3632cb8d84
Saif Uddin <admin@saifuddin.uk>
2024-02-11 12:38:09 +0000

    commenting in logic for authenticate login and signupNewUser funcitons

--- src/database.js
+++ src/database.js
@@ -5,14 +5,26 @@ const userDB = {
 
 
 function signupNewUser (userInput){
-    console.log(...userInput);
+    // console.log(...userInput);
     // add user/pwd if not in database, else don't change anything
     userDB[userInput[0]] = !userDB[userInput[0]] ? userInput[1] : userDB[userInput[0]]
-    console.log(userDB);
+    //console.log(userDB);
+    if (userDB[userInput[0]] === userInput[1]){
+        // if sign up successful, authenticate login and redirect to successful login
+    }
 }
 
 function authenticateUser(userInput){
-    console.log(...userInput);
+    // console.log(...userInput);
+    if (userDB[userInput[0]] && userDB[userInput[1]]){
+        //redirect user to successful login
+    }
+    else if (userDB[userInput[0]] && !userDB[userInput[1]]){
+        //incorrect password message
+    }
+    else if (!userDB[userInput[0]]){
+        // user does not exist message
+    }
 }
 
 export {signupNewUser, authenticateUser}
