6965032938e812534cba3d41fb4aa5a09d1b4e1e
Saif Uddin <admin@saifuddin.uk>
2024-02-11 12:29:09 +0000

    add users to a database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,14 @@
 
+const userDB = {
+
+}
+
+
 function signupNewUser (userInput){
     console.log(...userInput);
+    // add user/pwd if not in database, else don't change anything
+    userDB[userInput[0]] = !userDB[userInput[0]] ? userInput[1] : userDB[userInput[0]]
+    console.log(userDB);
 }
 
 function authenticateUser(userInput){
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -72,10 +72,10 @@ export default function SignIn() {
               autoComplete="current-password"
               onChange={(e)=>{setPassword(e.target.value)}}
             />
-            <FormControlLabel
+            {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
-            />
+            /> */}
             <Button
               type="submit"
               fullWidth
