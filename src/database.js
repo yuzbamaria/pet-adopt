f9be8b2da6a7944b13ad546347ea513f40210684
Saif Uddin <admin@saifuddin.uk>
2024-02-12 17:02:28 +0000

    updated logic for login

--- src/database.js
+++ src/database.js
@@ -3,7 +3,9 @@
 
 // const toggleLogin = useProfileUpdateContext();
 
-let userDB = {}
+let userDB = {
+    userAccounts:{},
+}
 
 function checkLocal(){
     userDB = (localStorage.getItem("skills-tracker")) ? JSON.parse(localStorage.getItem("skills-tracker")) : userDB;
@@ -15,9 +17,9 @@ function signupNewUser (userInput){
     // console.log(...userInput);
     // add user/pwd if not in database, else don't change anything
     checkLocal()
-    userDB[userInput[0]] = !userDB[userInput[0]] ? userInput[1] : userDB[userInput[0]]
+    userDB["userAccounts"][userInput[0]] = !userDB["userAccounts"][userInput[0]] ? userInput[1] : userDB["userAccounts"][userInput[0]]
     //console.log(userDB);
-    if (userDB[userInput[0]] === userInput[1]){
+    if (userDB["userAccounts"][userInput[0]] === userInput[1]){
         // if sign up successful, authenticate login and redirect to successful login
         console.log("new user signed up!");
         localStorage.setItem("skills-tracker", JSON.stringify(userDB));
@@ -28,16 +30,16 @@ function signupNewUser (userInput){
 function authenticateUser(userInput){
     // console.log(...userInput);
     checkLocal()
-    if (userDB[userInput[0]] && userDB[userInput[1]]){
+    if (userDB["userAccounts"][userInput[0]] === userInput[1]){
         console.log("successful login")
         localStorage.setItem("skills-tracker", JSON.stringify(userDB));
         return true;
     }
-    else if (userDB[userInput[0]] && !userDB[userInput[1]]){
+    else if (userDB["userAccounts"][userInput[0]] && userDB["userAccounts"][userInput[0]] !== userInput[1]){
         console.log("incorrect password!")
         //incorrect password message
     }
-    else if (!userDB[userInput[0]]){
+    else if (!userDB["userAccounts"][userInput[0]]){
         console.log("user does not exist!")
         // user does not exist message
     }
