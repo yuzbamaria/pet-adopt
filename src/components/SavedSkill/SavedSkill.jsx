52302e6e73b398117aca07a62f0db864d77a7ed8
Saif Uddin <admin@saifuddin.uk>
2024-02-12 16:10:43 +0000

    added saved skill component

--- src/components/SavedSkill/SavedSkill.jsx
+++ src/components/SavedSkill/SavedSkill.jsx
@@ -4,9 +4,9 @@ function SavedSkill(){
 
     return (
         <div className="saved-skill">
-            <h3 className="skill-title"></h3>
-            <p className="skill-status"></p>
-            <div className="progress"></div>
+            <h3 className="skill-title">skill title</h3>
+            <p className="skill-status">skill status</p>
+            <div className="progress">progress bar</div>
         </div>
     )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,16 @@
+import SavedSkill from "../components/SavedSkill/SavedSkill";
 import "./css/Profile.css"
 
 function Profile(){
     let loggedIn = false;
     return (
         <div className="profile">
-
+            <SavedSkill />
+            <SavedSkill />
+            <SavedSkill />
+            <SavedSkill />
+            <SavedSkill />
+            <SavedSkill />
         </div>
     )
 }
