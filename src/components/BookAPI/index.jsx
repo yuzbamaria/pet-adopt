4512928ec8068eb2c3ca558eaa5196e23ecc6cc4
Saif Uddin <admin@saifuddin.uk>
2024-02-14 15:41:36 +0000

    switching to desktop

--- src/components/BookAPI/index.jsx
+++ src/components/BookAPI/index.jsx
@@ -6,6 +6,7 @@ import { bookInfo } from "../../utils/api";
 
 function BookAPI(){
     const [searchTerm, setSearchTerm] = useState("");
+    const [books, setBooks] = useState(bookInfo);
 
     function handleChange(query){
         setSearchTerm(query);
@@ -16,31 +17,32 @@ function BookAPI(){
         console.log(searchTerm)
         setSearchQuery(searchTerm);
         callBookAPI();
+        setBooks(bookInfo);
     }
 
-    return(<>
-    <div>
-      <div>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e)=>handleChange(e.target.value)}
-          placeholder="Search Books"
-        />
-        <button className='btn btn-warning shadow m-2' onClick={searchBooks}>Search</button>
-      </div>
-      {/* List of books */}
-      <ul id="book-list">
-        {bookInfo.map((book) => (
-        <li key={book.url}>
-          <p>Title: {book.title}</p>
-          <p>Author(s): {book.authpr}</p>
-          <a href={book.url}>web-link</a>
-        </li>
-      ))}
-      </ul>
-    </div>
-    </>)
+    return(<div>
+        <div>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e)=>handleChange(e.target.value)}
+                placeholder="Search Books"
+            />
+            <button className='btn btn-warning shadow m-2' onClick={searchBooks}>Search</button>
+        </div>
+        {/* List of books */}
+        <ul id="book-list">
+            {/* {books.map((book) => ( */}
+            {bookInfo.map((book) => (
+            <li key={book.url}>
+                <div>
+                    <p>Title: {book.title}</p>
+                    <p>Author(s): {book.author}</p>
+                    <a href={book.url}>web-link</a>
+                </div>
+            </li>))}
+        </ul>
+    </div>)
 }
 
 export default BookAPI
