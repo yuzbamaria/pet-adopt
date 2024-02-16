6fe53af6fbecd3174f05d4a9677f1e3e9090cf95
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-02-16 13:30:50 +0200

    added animation component

---
+++ src/components/Animation/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css"
+
+function Animation() {
+    return ( <>
+        {/* <div className="home-container min-vh-100"> */}
+            <div className="wrapper">
+                <div className="scroll-text">
+                    <h1>Master Your Force</h1>
+                    <p>Padawan:</p>
+                    <p>This stage represents the beginning of the journey, where the individual is 
+                    learning the basics of the skill and undergoing training.</p>
+                    <p>Jedi Apprentice:</p>
+                    <p>Advancing from the Padawan stage, the individual begins to grasp more advanced 
+                    concepts and techniques, guided by a mentor or teacher.</p>
+                    <p>Jedi Knight:</p>
+                    <p>At this stage, the individual has mastered the fundamental skills and is capable 
+                    of using them effectively in various situations. They are more independent in their practice and may take on apprentices of their own.</p>
+                    <p>Jedi Master:</p>
+                    <p>The pinnacle of skill mastery, a Jedi Master is highly proficient in their chosen 
+                    area of expertise. They not only excel in using the skill but also have the wisdom and experience to guide others and contribute to the greater good.</p>
+                    <p>Jedi Grandmaster:</p>
+                    <p>This represents the rare few who have achieved unparalleled mastery, wisdom, and 
+                    leadership in their field. They are revered for their skill, knowledge, and contributions to the Jedi Order (or in this case, to the realm of skill development).</p>
+                </div>
+            </div>
+        {/* </div> */}
+    </>)
+}
+
+export default Animation;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css"
 
 const currentYear = new Date().getFullYear();
 
