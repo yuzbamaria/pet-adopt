6ac37398c2418ce31cc072f61e6a54578422be2e
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-02-16 19:23:48 +0200

    updated width

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -39,7 +39,7 @@ export default function SignIn() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth="xs" className="min-vh-100">
         <CssBaseline />
         <Box
           sx={{
